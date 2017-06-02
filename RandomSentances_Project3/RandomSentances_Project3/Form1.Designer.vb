e687d0e7f3a4d05d9f6dcf285538afa327b4e2db
Chandnipatel57 <chandnip057@gmail.com>
2017-06-02 11:59:49 -0500

    Add Random Sentance Generation project.

---
+++ RandomSentances_Project3/RandomSentances_Project3/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.btnNextSentence = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 18
+        Me.ListBox1.Location = New System.Drawing.Point(97, 91)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(624, 256)
+        Me.ListBox1.TabIndex = 0
+        '
+        'btnNextSentence
+        '
+        Me.btnNextSentence.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.btnNextSentence.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNextSentence.ForeColor = System.Drawing.Color.White
+        Me.btnNextSentence.Location = New System.Drawing.Point(97, 395)
+        Me.btnNextSentence.Name = "btnNextSentence"
+        Me.btnNextSentence.Size = New System.Drawing.Size(155, 42)
+        Me.btnNextSentence.TabIndex = 1
+        Me.btnNextSentence.Text = "Next Sentence"
+        Me.btnNextSentence.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.ForeColor = System.Drawing.Color.White
+        Me.btnClear.Location = New System.Drawing.Point(327, 395)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(155, 42)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "Clear Sentence"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.ForeColor = System.Drawing.Color.White
+        Me.btnExit.Location = New System.Drawing.Point(549, 395)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(155, 42)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(808, 510)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnNextSentence)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents btnNextSentence As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ RandomSentances_Project3/RandomSentances_Project3/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+    Dim strArticles() As String = {"the", "a", "an", "another", "some", "one"}
+    Dim strNouns() As String = {"computer", "bird", "girl", "person", "boy", "men", "animal", "processor", "knowledge", "women"}
+    Dim strPrepositions() As String = {"before", "after", "on", "by", "through", "around", "behind", "near", "over", "below", "above", "toward"}
+    Dim strAdjectives() As String = {"small", "good", "bad", "important", "useful", "helpful", "interesting", "right", "new", "public", "young"}
+    Dim strVerbs() As String = {"put", "used", "processed", "help", "contain", "check", "offer", "grow", "bring", "study"}
+
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ListBox1.Items.Clear()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnNextSentence_Click(sender As Object, e As EventArgs) Handles btnNextSentence.Click
+        Dim arrayindx As Integer
+        Dim rNumber As New Random
+
+        Dim NewGeneratedSentence As String = " "
+
+        arrayindx = rNumber.Next(0, strArticles.Length)
+        NewGeneratedSentence &= StrConv(strArticles(arrayindx), VbStrConv.ProperCase) & " "
+
+        arrayindx = rNumber.Next(0, strAdjectives.Length)
+        NewGeneratedSentence &= strAdjectives(arrayindx) & " "
+
+        arrayindx = rNumber.Next(0, strNouns.Length)
+        NewGeneratedSentence &= strNouns(arrayindx) & " "
+
+        arrayindx = rNumber.Next(0, strVerbs.Length)
+        NewGeneratedSentence &= strVerbs(arrayindx) & " "
+
+        arrayindx = rNumber.Next(0, strPrepositions.Length)
+        NewGeneratedSentence &= strPrepositions(arrayindx) & " "
+
+        arrayindx = rNumber.Next(0, strArticles.Length)
+        NewGeneratedSentence &= strArticles(arrayindx) & " "
+
+        arrayindx = rNumber.Next(0, strAdjectives.Length)
+        NewGeneratedSentence &= strAdjectives(arrayindx) & " "
+
+        arrayindx = rNumber.Next(0, strNouns.Length)
+        NewGeneratedSentence &= strNouns(arrayindx) & "."
+
+        ListBox1.Items.Add(NewGeneratedSentence)
+    End Sub
+End Class
