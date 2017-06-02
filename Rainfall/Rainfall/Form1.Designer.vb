35631757a25d9aec36ce21fd2cea8cd97ec660d2
Chandnipatel57 <chandnip057@gmail.com>
2017-06-02 17:57:12 -0500

    Add Rainfall Project. Which counts average rainfall for total twelve months.

---
+++ Rainfall/Rainfall/Form1.Designer.vb
@@ -0,0 +1,394 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.gb1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox12 = New System.Windows.Forms.TextBox()
+        Me.TextBox11 = New System.Windows.Forms.TextBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.dec = New System.Windows.Forms.Label()
+        Me.nov = New System.Windows.Forms.Label()
+        Me.oct = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.sep = New System.Windows.Forms.Label()
+        Me.jul = New System.Windows.Forms.Label()
+        Me.aug = New System.Windows.Forms.Label()
+        Me.jun = New System.Windows.Forms.Label()
+        Me.may = New System.Windows.Forms.Label()
+        Me.apr = New System.Windows.Forms.Label()
+        Me.feb = New System.Windows.Forms.Label()
+        Me.march = New System.Windows.Forms.Label()
+        Me.jan = New System.Windows.Forms.Label()
+        Me.btnc = New System.Windows.Forms.Button()
+        Me.btncl = New System.Windows.Forms.Button()
+        Me.btne = New System.Windows.Forms.Button()
+        Me.gb1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'gb1
+        '
+        Me.gb1.Controls.Add(Me.TextBox12)
+        Me.gb1.Controls.Add(Me.TextBox11)
+        Me.gb1.Controls.Add(Me.TextBox10)
+        Me.gb1.Controls.Add(Me.TextBox9)
+        Me.gb1.Controls.Add(Me.TextBox8)
+        Me.gb1.Controls.Add(Me.TextBox7)
+        Me.gb1.Controls.Add(Me.TextBox6)
+        Me.gb1.Controls.Add(Me.TextBox5)
+        Me.gb1.Controls.Add(Me.TextBox4)
+        Me.gb1.Controls.Add(Me.TextBox3)
+        Me.gb1.Controls.Add(Me.TextBox2)
+        Me.gb1.Controls.Add(Me.TextBox1)
+        Me.gb1.Controls.Add(Me.lbl2)
+        Me.gb1.Controls.Add(Me.lbl1)
+        Me.gb1.Controls.Add(Me.Label15)
+        Me.gb1.Controls.Add(Me.Label14)
+        Me.gb1.Controls.Add(Me.dec)
+        Me.gb1.Controls.Add(Me.nov)
+        Me.gb1.Controls.Add(Me.oct)
+        Me.gb1.Controls.Add(Me.Label10)
+        Me.gb1.Controls.Add(Me.sep)
+        Me.gb1.Controls.Add(Me.jul)
+        Me.gb1.Controls.Add(Me.aug)
+        Me.gb1.Controls.Add(Me.jun)
+        Me.gb1.Controls.Add(Me.may)
+        Me.gb1.Controls.Add(Me.apr)
+        Me.gb1.Controls.Add(Me.feb)
+        Me.gb1.Controls.Add(Me.march)
+        Me.gb1.Controls.Add(Me.jan)
+        Me.gb1.Location = New System.Drawing.Point(12, 12)
+        Me.gb1.Name = "gb1"
+        Me.gb1.Size = New System.Drawing.Size(474, 367)
+        Me.gb1.TabIndex = 0
+        Me.gb1.TabStop = False
+        Me.gb1.Text = "Rainfall Data"
+        '
+        'TextBox12
+        '
+        Me.TextBox12.Location = New System.Drawing.Point(350, 249)
+        Me.TextBox12.Name = "TextBox12"
+        Me.TextBox12.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox12.TabIndex = 20
+        '
+        'TextBox11
+        '
+        Me.TextBox11.Location = New System.Drawing.Point(350, 207)
+        Me.TextBox11.Name = "TextBox11"
+        Me.TextBox11.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox11.TabIndex = 19
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(350, 169)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox10.TabIndex = 18
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(350, 126)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox9.TabIndex = 18
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(350, 86)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox8.TabIndex = 17
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(350, 39)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox7.TabIndex = 17
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(102, 249)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox6.TabIndex = 16
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(102, 207)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 15
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(102, 166)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 14
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(102, 135)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 13
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(102, 86)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 13
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(102, 39)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 12
+        '
+        'lbl2
+        '
+        Me.lbl2.Location = New System.Drawing.Point(367, 335)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.Size = New System.Drawing.Size(83, 29)
+        Me.lbl2.TabIndex = 11
+        '
+        'lbl1
+        '
+        Me.lbl1.Location = New System.Drawing.Point(367, 288)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(83, 29)
+        Me.lbl1.TabIndex = 10
+        '
+        'Label15
+        '
+        Me.Label15.Location = New System.Drawing.Point(278, 288)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(70, 29)
+        Me.Label15.TabIndex = 9
+        Me.Label15.Text = "Total Rainfall"
+        '
+        'Label14
+        '
+        Me.Label14.Location = New System.Drawing.Point(243, 335)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(118, 29)
+        Me.Label14.TabIndex = 9
+        Me.Label14.Text = "Average Rainfall"
+        '
+        'dec
+        '
+        Me.dec.Location = New System.Drawing.Point(278, 249)
+        Me.dec.Name = "dec"
+        Me.dec.Size = New System.Drawing.Size(83, 29)
+        Me.dec.TabIndex = 8
+        Me.dec.Text = "December"
+        '
+        'nov
+        '
+        Me.nov.Location = New System.Drawing.Point(278, 207)
+        Me.nov.Name = "nov"
+        Me.nov.Size = New System.Drawing.Size(83, 29)
+        Me.nov.TabIndex = 8
+        Me.nov.Text = "November"
+        '
+        'oct
+        '
+        Me.oct.Location = New System.Drawing.Point(278, 169)
+        Me.oct.Name = "oct"
+        Me.oct.Size = New System.Drawing.Size(83, 29)
+        Me.oct.TabIndex = 7
+        Me.oct.Text = "October"
+        '
+        'Label10
+        '
+        Me.Label10.Location = New System.Drawing.Point(278, 169)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(83, 29)
+        Me.Label10.TabIndex = 7
+        Me.Label10.Text = "January"
+        '
+        'sep
+        '
+        Me.sep.Location = New System.Drawing.Point(278, 126)
+        Me.sep.Name = "sep"
+        Me.sep.Size = New System.Drawing.Size(83, 29)
+        Me.sep.TabIndex = 6
+        Me.sep.Text = "September"
+        '
+        'jul
+        '
+        Me.jul.Location = New System.Drawing.Point(278, 39)
+        Me.jul.Name = "jul"
+        Me.jul.Size = New System.Drawing.Size(83, 29)
+        Me.jul.TabIndex = 5
+        Me.jul.Text = "July"
+        '
+        'aug
+        '
+        Me.aug.Location = New System.Drawing.Point(278, 86)
+        Me.aug.Name = "aug"
+        Me.aug.Size = New System.Drawing.Size(83, 29)
+        Me.aug.TabIndex = 5
+        Me.aug.Text = "August"
+        '
+        'jun
+        '
+        Me.jun.Location = New System.Drawing.Point(35, 249)
+        Me.jun.Name = "jun"
+        Me.jun.Size = New System.Drawing.Size(83, 29)
+        Me.jun.TabIndex = 4
+        Me.jun.Text = "June"
+        '
+        'may
+        '
+        Me.may.Location = New System.Drawing.Point(35, 207)
+        Me.may.Name = "may"
+        Me.may.Size = New System.Drawing.Size(83, 29)
+        Me.may.TabIndex = 3
+        Me.may.Text = "May"
+        '
+        'apr
+        '
+        Me.apr.Location = New System.Drawing.Point(35, 169)
+        Me.apr.Name = "apr"
+        Me.apr.Size = New System.Drawing.Size(83, 29)
+        Me.apr.TabIndex = 2
+        Me.apr.Text = "April"
+        '
+        'feb
+        '
+        Me.feb.Location = New System.Drawing.Point(35, 86)
+        Me.feb.Name = "feb"
+        Me.feb.Size = New System.Drawing.Size(83, 29)
+        Me.feb.TabIndex = 1
+        Me.feb.Text = "February"
+        '
+        'march
+        '
+        Me.march.Location = New System.Drawing.Point(35, 126)
+        Me.march.Name = "march"
+        Me.march.Size = New System.Drawing.Size(83, 29)
+        Me.march.TabIndex = 1
+        Me.march.Text = "March"
+        '
+        'jan
+        '
+        Me.jan.Location = New System.Drawing.Point(35, 39)
+        Me.jan.Name = "jan"
+        Me.jan.Size = New System.Drawing.Size(83, 29)
+        Me.jan.TabIndex = 0
+        Me.jan.Text = "January"
+        '
+        'btnc
+        '
+        Me.btnc.Location = New System.Drawing.Point(17, 385)
+        Me.btnc.Name = "btnc"
+        Me.btnc.Size = New System.Drawing.Size(113, 43)
+        Me.btnc.TabIndex = 1
+        Me.btnc.Text = "&Calculate"
+        Me.btnc.UseVisualStyleBackColor = True
+        '
+        'btncl
+        '
+        Me.btncl.Location = New System.Drawing.Point(199, 385)
+        Me.btncl.Name = "btncl"
+        Me.btncl.Size = New System.Drawing.Size(113, 43)
+        Me.btncl.TabIndex = 2
+        Me.btncl.Text = "C&lear"
+        Me.btncl.UseVisualStyleBackColor = True
+        '
+        'btne
+        '
+        Me.btne.Location = New System.Drawing.Point(362, 385)
+        Me.btne.Name = "btne"
+        Me.btne.Size = New System.Drawing.Size(113, 43)
+        Me.btne.TabIndex = 3
+        Me.btne.Text = "Exit"
+        Me.btne.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(501, 438)
+        Me.Controls.Add(Me.btne)
+        Me.Controls.Add(Me.btncl)
+        Me.Controls.Add(Me.btnc)
+        Me.Controls.Add(Me.gb1)
+        Me.Name = "Form1"
+        Me.Text = "Rainfall Statistics"
+        Me.gb1.ResumeLayout(False)
+        Me.gb1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents gb1 As System.Windows.Forms.GroupBox
+    Friend WithEvents jan As System.Windows.Forms.Label
+    Friend WithEvents TextBox12 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox11 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox10 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox9 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox8 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents lbl2 As System.Windows.Forms.Label
+    Friend WithEvents lbl1 As System.Windows.Forms.Label
+    Friend WithEvents Label15 As System.Windows.Forms.Label
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents dec As System.Windows.Forms.Label
+    Friend WithEvents nov As System.Windows.Forms.Label
+    Friend WithEvents oct As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents sep As System.Windows.Forms.Label
+    Friend WithEvents jul As System.Windows.Forms.Label
+    Friend WithEvents aug As System.Windows.Forms.Label
+    Friend WithEvents jun As System.Windows.Forms.Label
+    Friend WithEvents may As System.Windows.Forms.Label
+    Friend WithEvents apr As System.Windows.Forms.Label
+    Friend WithEvents feb As System.Windows.Forms.Label
+    Friend WithEvents march As System.Windows.Forms.Label
+    Friend WithEvents btnc As System.Windows.Forms.Button
+    Friend WithEvents btncl As System.Windows.Forms.Button
+    Friend WithEvents btne As System.Windows.Forms.Button
+
+End Class
---
+++ Rainfall/Rainfall/Form1.vb
@@ -0,0 +1,61 @@
+﻿Public Class Form1
+
+    Private Sub btnc_Click(sender As Object, e As EventArgs) Handles btnc.Click
+        Dim dscore1 As Byte
+        Dim dscore2 As Byte
+        Dim dscore3 As Byte
+        Dim dscore4 As Byte
+        Dim dscore5 As Byte
+        Dim dscore6 As Byte
+        Dim dscore7 As Byte
+        Dim dscore8 As Byte
+        Dim dscore9 As Byte
+        Dim dscore10 As Byte
+        Dim dscore11 As Byte
+        Dim dscore12 As Byte
+        Dim dtotal As Double
+        Dim davg As Double
+        Try
+            dscore1 = CByte(TextBox1.Text)
+            dscore2 = CByte(TextBox2.Text)
+            dscore3 = CByte(TextBox3.Text)
+            dscore4 = CByte(TextBox4.Text)
+            dscore5 = CByte(TextBox5.Text)
+            dscore6 = CByte(TextBox6.Text)
+            dscore7 = CByte(TextBox7.Text)
+            dscore8 = CByte(TextBox8.Text)
+            dscore9 = CByte(TextBox9.Text)
+            dscore10 = CByte(TextBox10.Text)
+            dscore11 = CByte(TextBox11.Text)
+            dscore12 = CByte(TextBox12.Text)
+            dtotal = dscore1 + dscore2 + dscore3 + dscore4 + dscore5 + dscore6 + dscore7 + dscore8 + dscore9 + dscore10 + dscore11 + dscore12
+            davg = dtotal / 12
+            lbl1.Text = dtotal.ToString("n2")
+            lbl2.Text = davg.ToString("n2")
+
+        Catch ex As Exception
+            MessageBox.Show("Data Must be NUMERIC.! Try Again")
+        End Try
+    End Sub
+
+    Private Sub btncl_Click(sender As Object, e As EventArgs) Handles btncl.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+        TextBox6.Text = ""
+        TextBox7.Text = ""
+        TextBox8.Text = ""
+        TextBox9.Text = ""
+        TextBox10.Text = ""
+        TextBox11.Text = ""
+        TextBox12.Text = ""
+        lbl1.Text = ""
+        lbl2.Text = ""
+    End Sub
+
+    Private Sub btne_Click(sender As Object, e As EventArgs) Handles btne.Click
+        Me.Close()
+    End Sub
+End Class
